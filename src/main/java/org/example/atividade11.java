e202e67552a6a63e84fc3dbe1539c0ad09205bf3
Juliano Vieira Frazão <65692712+JulianoFrazao0@users.noreply.github.com>
2026-09-08 20:10:32 -0400

    saborrr

--- src/main/java/org/example/atividade11.java
+++ src/main/java/org/example/atividade11.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 
 public class atividade11 {
     public static void main(String[]args) {
-
-        Scanner sc = new Scanner(String.in);
+        System.out.println("s");
         
     }
 }
